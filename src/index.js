abf4f942dfaed2253768205a1cf9d0bd7c095d3f
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-04-08 21:50:15 -0400

    added react components

--- src/index.js
+++ src/index.js
@@ -1,31 +1,105 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+// function formatName(user) {
+//     return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+// }
+
+// function tick() {
+//     const element = (
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//         </div>
+//     ); 
+
+// ReactDOM.render(element, document.getElementById('root'));
+
+// }
+
+// setInterval(tick, 1000);
+
+// class Welcome extends React.Component {
+//     render() {
+//         return <h1>Hello, {this.props.name}</h1>;
+//     }
+// }
+
+// const element = <Welcome name="Andre" />;
+
+// function App() {
+//     return (
+//         <div>
+//             <Avatar name="Sara"/>
+//             <Comment name="Cahal"/>
+//             <UserInfo name="Edita"/>
+//         </div>
+//     );
+// }
+
+function formatDate(date) {
+    return date.toLocaleDateString();
+  }
+
+function Avatar(props) {
+    return (
+        <img className="Avater"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+            />
+    );
 }
 
-const user = {
-    firstName: 'John',
-    lastName: 'Doe'
-}
-
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
         </div>
-    ); 
-
-ReactDOM.render(element, document.getElementById('root'));
-
+    );
 }
 
-setInterval(tick, 1000);
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+  };
+  ReactDOM.render(
+    <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}
+    />,
+    document.getElementById('root')
+  );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
