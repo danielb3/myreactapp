abd89d7e8a39bcec943d794cef34efc1b2625ce5
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-04-18 18:44:24 -0400

    text edits

--- src/Contact.js
+++ src/Contact.js
@@ -19,7 +19,7 @@ class Contact extends React.Component {
         return (
             <div style={styles.background}>
                 <h1 id="contact" className="wow fadeIn" data-wow-duration="3s" data-wow-delay=".5s" data-wow-offset="20" style={styles.title}>Contact Me</h1>
-                <p className="wow fadeIn" data-wow-duration="3s" data-wow-delay=".5s" data-wow-offset="20" style={styles.title}><b>I'm currently looking for a position where I'll be working with websites.  Drop me a line if you would like to chat.</b></p>
+                <p className="wow fadeIn" data-wow-duration="3s" data-wow-delay=".5s" data-wow-offset="20" style={styles.title}><b>I'm currently looking for a web programming job.  If you like my work, drop me a line to chat.</b></p>
                 <p className="wow fadeIn" data-wow-duration="3s" data-wow-delay=".5s" data-wow-offset="20" style={styles.title}>Email: danielbutton001 [at] gmail [dot] com</p>
                 <p className="wow fadeIn" data-wow-duration="3s" data-wow-delay=".5s" data-wow-offset="20" style={styles.title}>Phone: two one six - two one two - two nine five nine</p>
                 <p className="wow fadeIn" data-wow-duration="3s" data-wow-delay=".5s" data-wow-offset="20" style={styles.title}>Location: Greater Washington, DC Region</p>
